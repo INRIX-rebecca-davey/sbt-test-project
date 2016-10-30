fb057a3319124a978dda617cc595f639151130d9
Rebecca Davey <rebecca.davey@inrix.com>
2016-10-30 21:07:16 +0000

    cut back to just slides, no code

--- src/main/scala/Ingredient.scala
+++
@@ -1,7 +0,0 @@
-trait Ingredient {
-  val name: String
-  val quantity: Int
-}
-
-case class Liquid(name: String, quantity: Int) extends Ingredient
-case class Powder(name: String, quantity: Int) extends Ingredient
